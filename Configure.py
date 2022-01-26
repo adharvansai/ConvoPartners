abfab6f349d8cd97f7d01c6a25a11010e3a49751
adharvansai <adhar1sai@tamu.edu>
2022-01-26 13:25:28 -0600

    "Initial Data Pre-processing"

---
+++ Configure.py
@@ -0,0 +1,89 @@
+# Configuration file
+
+# *************************************************************************************************************
+
+# NON NATIVE SPEAKERS
+
+# =============================================
+
+# File path of the processed excel sheet according to the below given instruction set. Please include .xlsx in the file path
+
+int_filepath = ""
+
+# =============================================
+
+# Please make sure the names of the columns are correctly spelled according to the data sheets
+
+icols = [
+    'Training Timestamp',
+    'Timestamp',
+    'Email Address',
+    'First and Last Name',
+    'UIN',
+    'Please select your preference for meetings with your conversation partner.',
+    'What times are you available? [9 AM]',
+    'What times are you available? [10 AM]',
+    'What times are you available? [11 AM]',
+    'What times are you available? [12 PM]',
+    'What times are you available? [1 PM]',
+    'What times are you available? [2 PM]',
+    'What times are you available? [3 PM]',
+    'What times are you available? [4 PM]',
+    'What times are you available? [5 PM]',
+    'What times are you available? [6 PM]',
+    'What times are you available? [7 PM]',
+    'What times are you available? [8 PM]'
+]
+
+# =============================================
+
+# *************************************************************************************************************
+
+# NON NATIVE SPEAKERS
+
+# =============================================
+
+# File path of the processed excel sheet according to the below given instruction set. Please include .xlsx in the file path
+
+amc_filepath = ""
+
+# =============================================
+
+# Please make sure the names of the columns are correctly spelled according to the data sheets
+
+acols = [
+    'Training Timestamp',
+    'Timestamp',
+    'Email Address',
+    'First and Last Name',
+    'UIN',
+    'If you are interested in a Conversation Partnership, please select your preference for meeting.',
+    'What times are you available? [9 AM]',
+    'What times are you available? [10 AM]',
+    'What times are you available? [11 AM]',
+    'What times are you available? [12 PM]',
+    'What times are you available? [1 PM]',
+    'What times are you available? [2 PM]',
+    'What times are you available? [3 PM]',
+    'What times are you available? [4 PM]',
+    'What times are you available? [5PM]',
+    'What times are you available? [6PM]',
+    'What times are you available? [7PM]',
+    'What times are you available? [8PM]',
+    # -- '[Conversation Partnership (10 weekly conversations; one half-hour per week)]'
+]
+
+#*******************************************************************************************************************
+
+# Constant Configurations
+
+week = {
+    'Monday' : 1,
+    'Tuesday' : 2,
+    'Wednesday' : 3,
+    'Thursday' : 4,
+    'Friday' : 5,
+    'Saturday' : 6,
+    'Sunday' : 7
+}
+
--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print("Testing")
+import pandas as pd
+from Configure import icols
+
+test = pd.read_excel("/Users/adharvan/PycharmProjects/ConvoPartners/imbp.xlsx")
+s = 'Please select your preference for meetings with your conversation partner.'
+test[s] = test[s].replace(to_replace ='.*[zZ]oom.*', value = 3, regex = True)
+test[s] = test[s].replace(to_replace ='.*In-person.*', value = 2, regex = True)
+test[s] = test[s].replace(to_replace ='.*No [pP]reference.*', value = 1, regex = True)
+#print(test['Training Timestamp'])
+tnp = test.to_numpy()
+tnpr= test[icols[6:]]
+tnpl = test[icols[:6]]
+tnpl["Time Slots"] = ""
+#print(tnpr[icols[6]][2])
+tnpr.fillna("")
+for i in range(10):
+    for j in range(6, len(icols)):
+        col = icols[j]
+        ts = tnpr[col][i]
+        #ts = ts.split(",")
+        print(ts)
+
+    print("="*30)
+#print(tnpl)
+
+
